6500988021ba8197fbab968909c86c7803c5540d
Beneditova <krisbeneditova@gmail.com>
2019-09-21 21:21:24 +0300

    dice dot possitioning fixed and the js created div has a class assigned to it

--- Dicee Game/ui.js
+++ Dicee Game/ui.js
@@ -4,25 +4,21 @@ function printNumber(number) {
 }
 
 var button = document.getElementById("button");
+var result;
 
 button.onclick = function() {
-  var result = dice.roll();
+   result = dice.roll();
   printNumber(result);
+
+};
+console.log(result);
+
+var createDiceElement = function (result) {
+
+  var firstDice = document.createElement("div");
+  firstDice.classList.add("firstDot");
+
+  return firstDice;
 };
 
-var createDiceElement = function () {
-  var div = document.createElement("div");
-  div.style.width = "58px";
-  div.style.height = "54px";
-  div.style.background = "red";
-  div.style.borderRadius = "64%";
-  div.style.color = "white";
-  div.style.marginLeft = "48%";
-  div.style.marginTop= "-13%";
-  div.innerHTML = "Hello";
-
-  return div;
-};
-
-
-document.getElementById("main").appendChild(createDiceElement());
+  document.getElementById("main").appendChild(createDiceElement(result));
