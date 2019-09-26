092e591c9e2ceb810d1bfe1a6806fd2cd9cdeb11
Beneditova <krisbeneditova@gmail.com>
2019-09-26 13:28:25 +0300

    dice generator function added

---
+++ Dice Roller/js/diceGenerate.js
@@ -0,0 +1,17 @@
+var dice = {
+sides: 6,
+roll: function () {
+  var sides = 6;
+  var randomNumber = Math.floor(Math.random() * this.sides) + 1;
+  return (randomNumber);
+}};
+
+function Dice(sides) {
+  this.sides = sides;
+}
+
+Dice.prototype.roll = function diceRoll () {
+  var randomNumber = Math.floor(Math.random() * this.sides) + 1;
+    return randomNumber;
+  };
+var dice = new Dice(6);
