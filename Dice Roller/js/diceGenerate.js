a5632a8310d042178000d646a319525f61730359
Beneditova <krisbeneditova@gmail.com>
2019-10-01 17:07:56 +0300

    the for loop now goes through all the dots in the die

--- Dice Roller/js/diceGenerate.js
+++ Dice Roller/js/diceGenerate.js
@@ -1,23 +1,44 @@
-var dice = {
-sides: 6,
-roll: function () {
-  var sides = 6;
-  var randomNumber = Math.floor(Math.random() * this.sides) + 1;
-  return (randomNumber);
-}};
+
 
 function Dice(sides) {
   this.sides = sides;
 }
 
 Dice.prototype.roll = function diceRoll () {
-  var randomNumbersHolder;
+  var randomNumbersHolder= [];
 
-  for (var i = 0; i < array.length; i++) {
-    randomNumbersHolder.push(Math.floor(Math.random() * this.sides) + 1);
+  for (var i = 0; i < this.sides.length; i++) {
+    randomNumbersHolder.push(Math.floor(Math.random() * 10));
   }
 
     return randomNumbersHolder;
   };
 
 var dice = new Dice(6);
+
+
+function getDot(number) {
+  var placeholder = document.getElementById("dice_dot_"+number);
+  placeholder.innerHTML = number;
+}
+
+var button = document.getElementById("button");
+
+
+button.onclick = function() {
+  var result = dice.roll();
+
+  return result;
+  };
+
+
+var allDice = document.getElementsByClassName("yahtzee_die");
+
+for(i = 0; i < allDice.length;i++)
+{
+  for (var j = 0; j < button.onclick().length; j++) {
+    allDice[i].style.color = 'red';
+  }
+}
+
+//getElement => on doc ready
