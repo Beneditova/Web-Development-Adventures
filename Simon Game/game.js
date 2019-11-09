029db717bb3a1a0355c4a83ee9076d2e3d08b96b
Beneditova <krisbeneditova@gmail.com>
2019-11-09 20:32:00 +0200

    button click has a sound now

--- Simon Game/game.js
+++ Simon Game/game.js
@@ -1,33 +1,49 @@
-class SimonGame{
-  constructor(){
+class SimonGame {
+  constructor() {
     this.buttonColours = ["red", "blue", "green", "yellow"];
-    
+
     this.gamePattern = [];
 
     this.userClickedPattern = [];
-   
-    $(".btn").click((function(){
-       let self = this
-       var userChosenColour = $(this).attr("id");
-      
-       self.userClickedPattern.push(userChosenColour);
+
+    self = this;
+    $(".btn").click((function () {
     
-       console.log("userClickedPattern");
-      }));
+      var userChosenColour = $(this).attr("id");
+
+      self.userClickedPattern.push(userChosenColour);
+
+      console.log("userClickedPattern");
+    }));
   }
 
-   nextSequence() {
+  nextSequence() {
 
     var randomNumber = Math.floor(Math.random() * 4);
     var randomChosenColour = this.buttonColours[randomNumber];
-   
+
     this.gamePattern.push(randomChosenColour);
-  
-   $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
-  
-    
+
+    $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+
+
     var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
     audio.play();
+  }
+
+  playSound(name) {
+    var audio = new Audio("sounds/" + name + ".mp3");
+    audio.play();
+  }
+
+
+  animatePress(currentColor) {
+
+    $("#" + currentColor).addClass("pressed");
+
+    setTimeout(function () {
+      $("#" + currentColor).removeClass("pressed");
+    }, 100);
   }
 }
 
