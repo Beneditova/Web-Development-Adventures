67530ded6e2ed71aad14ad573720ceaa2ff342f7
Beneditova <krisbeneditova@gmail.com>
2019-11-21 23:10:02 +0200

    you can add ingredients to the burger such ass meat or cheese by pressing a button

--- burger-builder/src/components/Burger/Burger.js
+++ burger-builder/src/components/Burger/Burger.js
@@ -4,12 +4,18 @@ import BurgerIgnredient from './BurgerIngredient/BurgerIngredient'
 
 const burger = (props) => {
   
-    const transformedIngredients = Object.keys(props.ingredients).map(ingredientKey =>{
+    let transformedIngredients = Object.keys(props.ingredients).map(ingredientKey =>{
         return [...Array(props.ingredients[ingredientKey])].map((_,i) =>{
           return  <BurgerIgnredient key = {ingredientKey + i} type={ingredientKey} />;
         });
-    });
-  
+    })
+    .reduce((arr, el) => {
+        return arr.concat(el)
+    }, []);
+ 
+    if(transformedIngredients.length === 0){
+    transformedIngredients = <p> Pls add some ingredients</p>
+  }
     return (
         <div className={classes.Burger}>
             <BurgerIgnredient type="bread-top" />
