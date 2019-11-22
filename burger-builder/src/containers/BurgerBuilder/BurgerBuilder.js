fa8f672ca830b096024f3e1ca509c2c4e276e962
Beneditova <krisbeneditova@gmail.com>
2019-11-22 20:41:53 +0200

    updatePurchaseState method created to check whatever there is an ingredient in the burger so that the order makes sense you can't buy just bread right

--- burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -10,62 +10,81 @@ const INGREDIENT_PRICES = {
     bacon: 0.7
 }
 
-class BurgerBuilder extends Component{
+class BurgerBuilder extends Component {
     state = {
-            ingredients: {
-                salad:0,
-                bacon: 0,
-                cheese: 0,
-                meat: 0
-          },
-          totalPrice: 4
+        ingredients: {
+            salad: 0,
+            bacon: 0,
+            cheese: 0,
+            meat: 0
+        },
+        totalPrice: 4,
+        purchasable: false
     }
 
-    addIgnredientHandler = (type) =>{
+    updatePurchaseState(ingredients) {
+        const sum = Object.keys(ingredients)
+        .map(ingredientKey =>{
+            return ingredients[ingredientKey]
+        })
+        .reduce((sum, el) => {
+        return sum + el;
+        }, 0);
+
+        this.setState({purchasable: sum > 0});
+    }
+
+    addIgnredientHandler = (type) => {
         const oldCount = this.state.ingredients[type];
-        const updatedCount= oldCount+1;
+        const updatedCount = oldCount + 1;
         const updatedIngredients = {
             ...this.state.ingredients
         };
-      
-        updatedIngredients[type]=updatedCount;
+
+        updatedIngredients[type] = updatedCount;
 
         const priceAddition = INGREDIENT_PRICES[type];
         const oldPrice = this.state.totalPrice;
         const newPrice = oldPrice + priceAddition;
-        this.setState({totalPrice: newPrice, ingredients: updatedIngredients});
+        this.setState({ totalPrice: newPrice, ingredients: updatedIngredients });
+        this.updatePurchaseState(updatedIngredients);
     }
 
-    removeIgnredientHandler = (type) =>{
+    removeIgnredientHandler = (type) => {
         const oldCount = this.state.ingredients[type];
-        const updatedCount= oldCount-1;
+        const updatedCount = oldCount - 1;
         const updatedIngredients = {
             ...this.state.ingredients
         };
-      
-        updatedIngredients[type]=updatedCount;
+
+        updatedIngredients[type] = updatedCount;
 
         const priceAddition = INGREDIENT_PRICES[type];
         const oldPrice = this.state.totalPrice;
         const newPrice = oldPrice - priceAddition;
-        this.setState({totalPrice: newPrice, ingredients: updatedIngredients});
+        this.setState({ totalPrice: newPrice, ingredients: updatedIngredients });
+        this.updatePurchaseState(updatedIngredients);
     }
 
-    render(){
-        const disableInfo ={
+    render() {
+        const disableInfo = {
             ...this.state.ingredients
         };
-        for(let key in disableInfo){
-            disableInfo[key] = disableInfo[key] <=0
+      
+        for (let key in disableInfo) {
+            disableInfo[key] = disableInfo[key] <= 0
         }
-        return(
+      
+        return (
             <Auxiliary>
-              <Burger ingredients={this.state.ingredients}/>
-              <BuildControls 
-              ingredientAdded={this.addIgnredientHandler} 
-              ingredientRemoved={this.removeIgnredientHandler} 
-              disabled={disableInfo}/>
-             </Auxiliary>
+                <Burger ingredients={this.state.ingredients} />
+                <BuildControls
+                    ingredientAdded = {this.addIgnredientHandler}
+                    ingredientRemoved = {this.removeIgnredientHandler}
+                    disabled = {disableInfo}
+                    purchasable = {this.state.purchasable}
+                    price = {this.state.totalPrice} />
+            </Auxiliary>
         );
     }
 }
