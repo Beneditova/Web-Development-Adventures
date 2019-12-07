c97e700cae88824e8b705ad92c2893f921981c1d
Beneditova <krisbeneditova@gmail.com>
2019-12-07 19:44:55 +0200

    routing added to the burger app in the BurgerBuilder.js file purchaseContinueHandler method was modified to load the Checkout component separately

--- Yeah React Projects/burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
+++ Yeah React Projects/burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -73,28 +73,16 @@ class BurgerBuilder extends Component {
     }
     
     purchaseContinueHandler = () =>{
-        this.setState({ loading: true});
-        const order ={
-            ingredients: this.state.ingredients,
-            price: this.state.totalPrice,
-            customer: {
-                name: 'Kris',
-                address: {
-                    street: 'TestStreet',
-                    zipCode: '4001',
-                    country: 'Bulgaria'
-                },
-                email: 'test@email.com'
-            },
-            deliveryMethod: 'fastest'
+        const queryParams = [];
+        for (let i in this.state.ingredients) {
+            queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]));
         }
-        axios.post('/orders.json', order)
-        .then(response => {
-             this.setState({ loading: true, purchasing: false})
-            })
-        .catch(error=> {
-            this.setState({ loading: true, purchasing: false})
-           });
+        queryParams.push('price=' + this.state.totalPrice);
+        const queryString = queryParams.join('&');
+        this.props.history.push({
+            pathname: '/checkout',
+            search: '?' + queryString
+        });
     }
 
     removeIgnredientHandler = (type) => {
