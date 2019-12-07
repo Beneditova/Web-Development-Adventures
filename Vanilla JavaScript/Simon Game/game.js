85cdb266b6d0149fc8ca672ae86f601978844f59
Beneditova <krisbeneditova@gmail.com>
2019-12-07 20:26:07 +0200

    checkAnswer method added to check what has the user pressed and advance to the next level the user plays till a wrong answer is given =>simon game task completed

--- Vanilla JavaScript/Simon Game/game.js
+++ Vanilla JavaScript/Simon Game/game.js
@@ -8,6 +8,18 @@ class SimonGame {
 
     this.userClickedPattern = [];
     
+    this.started = false; 
+    
+    this.level = 0;
+    
+    $(document).keypress(function() {
+      if (!self.started) {
+        $("#level-title").text("Level " + self.level);
+        self.nextSequence();
+        self.started = true;
+      }
+    });
+
     $(".btn").click((function () {
     
       var userChosenColour = $(this).attr("id");
@@ -16,20 +28,22 @@ class SimonGame {
 
       self.playSound(userChosenColour);
       self.animatePress(userChosenColour);
-    }));
+      self.checkAnswer(self.userClickedPattern.length-1);
+     }));
   }
 
   nextSequence() {
+    this.userClickedPattern = [];
 
+    this.level++;
+    $("#level-title").text("Level " + this.level);
+  
     var randomNumber = Math.floor(Math.random() * 4);
     var randomChosenColour = this.buttonColours[randomNumber];
-
     this.gamePattern.push(randomChosenColour);
-
+  
     $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
-
-    var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
-    audio.play();
+    this.playSound(randomChosenColour);
   }
 
   playSound(name) {
@@ -45,8 +59,22 @@ class SimonGame {
       $("#" + currentColor).removeClass("pressed");
     }, 100);
   }
+ 
+  checkAnswer(currentLevel) {
+   if (this.gamePattern[currentLevel] === this.userClickedPattern[currentLevel]) {
+
+    console.log("success");
+     if (this.userClickedPattern.length === this.gamePattern.length){
+        setTimeout( () => {
+        this.nextSequence();
+      }, 1000);
+
+    }
+   } else {
+     console.log("wrong");
+     $("#level-title").text("You Have Failed Try Again!");
+    }
+  }
 }
-
-
 
 
